f3452d7aaa527b82f05cd86059a3cdbbd1b0b245
aambroo <ambrosini.matteo21@gmail.com>
2022-06-07 18:14:58 +0200

    ✨ENHANCEMENT: detectFeatures now manages color correction

--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,14 @@
 import cv2
 import numpy
 
+# Formats
 IMG_FORMATS = ['png', 'jpg', 'jpeg', 'tiff']
 VIDEO_FORMATS = ['mp4', 'aac']
+# Detectors
 DETECTORS = ['SIFT', 'SURF', 'ORB']
 
 
-def imgDetectFeatures (path_to_img: str, detector: str) -> numpy.ndarray:
+def detectFeatures (path_to_img: str, detector: str, color_correction: bool) -> numpy.ndarray:
     """
     Given :param path_to_img: performs feature detection based on
     the specified :param detector:
@@ -30,7 +32,12 @@ def imgDetectFeatures (path_to_img: str, detector: str) -> numpy.ndarray:
                                           f'Please choose between {DETECTORS}!')
     else:
         raise f"Please choose a detector between {DETECTORS}!"
-    img = cv2.imread(path_to_img)                           # Reading Image
+
+    if color_correction:
+        img = cv2.imread(path_to_img, cv2.COLOR_BGR2GRAY)   # Apply Color Correction
+    else:
+        img = cv2.imread(path_to_img)                       # Reading Image
+
     kp, descriptors = model.detectAndCompute(img, None)     # Saving KeyPoints
     img_out = cv2.drawKeypoints(img, kp, None)              # Spit Out Image
     return img_out
