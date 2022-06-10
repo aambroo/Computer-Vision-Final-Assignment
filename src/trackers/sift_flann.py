c674fac9ae5f46818d5d033e0b6f3471e0596219
aambroo <ambrosini.matteo21@gmail.com>
2022-06-10 21:57:14 +0200

    🆕 Added detector + matcher files

--- src/trackers/sift_flann.py
+++ src/trackers/sift_flann.py
@@ -1,37 +1,32 @@
 import cv2
-from src.utils.GLOBALS import ORB_SEARCH_PARAMS, ORB_INDEX_PARAMS, MRT, MATCH_COLOR, KP_COLOR
+from src.utils.GLOBALS import SIFT_INDEX_PARAMS, SIFT_SEARCH_PARAMS, MRT, KP_COLOR, MATCH_COLOR
 import src.utils.color_coding as cc
 
-orb = cv2.ORB_create()
-flann = cv2.FlannBasedMatcher(ORB_INDEX_PARAMS, ORB_SEARCH_PARAMS)
+sift = cv2.SIFT_create(1000)
+flann = cv2.FlannBasedMatcher(SIFT_INDEX_PARAMS, SIFT_SEARCH_PARAMS)
 
 cap = cv2.VideoCapture('../../data/Contesto_industriale1.mp4')
 
 prev_kp = None
 prev_des = None
-frames_count = 0  # init frame_count
+frames_count = 0    # init frame_count
 num_detections = 0  # init num_detections
 
 while cap.isOpened():
 
     ret, frame = cap.read()
     frame = cc.make_BW(frame)
-    kp, des = orb.detectAndCompute(frame, None)
+    kp, des = sift.detectAndCompute(frame, None)
 
     if prev_kp is None:
         pass
     else:
-        matches = flann.knnMatch(prev_des, des, k=2)
+        matches = flann.knnMatch(des, prev_des, k=2)
 
         # Use mask to only draw good matches -> replaces good_matches list
-        matches_mask = [[0, 0] for _ in range(len(matches))]
+        matches_mask = [[0, 0] for i in range(len(matches))]
 
-        # Addresses an issue due to possible miss-pairing of matches
-        for idx, pair in enumerate(matches):
-            try:
-                i, j = pair
-            except:
-                continue
+        for idx, (i, j) in enumerate(matches):
             if i.distance < MRT * j.distance:
                 matches_mask[idx] = [1, 0]
 
@@ -42,12 +37,12 @@ while cap.isOpened():
             flags=cv2.DrawMatchesFlags_DEFAULT,
         )
         img = cv2.drawMatchesKnn(
-            prev_frame, prev_kp,
             frame, kp,
+            prev_frame, prev_kp,
             matches, None,
-            matchesMask=matches_mask
+            **draw_params
         )
-        cv2.imshow('ORB + FLANN', img)
+        cv2.imshow('SIFT + FLANN', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
