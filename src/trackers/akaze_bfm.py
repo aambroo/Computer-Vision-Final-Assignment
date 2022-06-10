f200cc4b4a8152a221815e5c2b96a49c65bdde7e
aambroo <ambrosini.matteo21@gmail.com>
2022-06-10 15:34:52 +0200

    🪲BUGFIX: fixed bugs in parameter parsing

--- src/trackers/akaze_bfm.py
+++ src/trackers/akaze_bfm.py
@@ -1,64 +1,43 @@
 import cv2
-import matplotlib.pyplot as plt
-import src.utils.feature_detection as fd
+import src.utils.color_coding as cc
 
+akaze = cv2.AKAZE_create()
+bf = cv2.BFMatcher()
 
 cap = cv2.VideoCapture("../../data/Contesto_industriale1.mp4")
 
-draw_params = dict(
-    matchColor=(0, 255, 0),
-    singlePointColor=(255, 0, 0),
-    flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
-)
-
-last_frame_features = None
-last_frame_image = None
-
-descriptor = fd.get_detector('sift', 1000)
-matcher = fd.get_matcher('bf', cv2.NORM_L2)
+prev_kp = None
+prev_des = None
 
 while cap.isOpened():
 
     ret, frame = cap.read()
-    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cc.make_BW(frame)
+    kp, des = akaze.detectAndCompute(frame, None)
 
-    kp, des = descriptor.detectAndCompute(gray_image, None)
-    print(des)
-    if last_frame_features is not None and des is not None:
-        matches = matcher.knnMatch(last_frame_features, des, k=2)
+    if prev_kp is None:
+        pass
+    else:
+        matches = bf.knnMatch(des, prev_des, k=2)
 
-        # Ratio Test
-        # good_matches = fd.find_good_matches_knn(
-        #     matcher=matcher,
-        #     des1=last_frame_features,
-        #     des2=des)
-        # Lowe's Ratio Test
-        good_matches = fd.find_good_matches_Lowe(matches)
-        draw_params = dict(singlePointColor=(255, 0, 0),
-                           matchesMask=good_matches,
-                           flags=cv2.DrawMatchesFlags_DEFAULT)
-        img = fd.draw_good_matches_Lowe(
-            last_frame_image, last_frame_kp,
+        good_matches = []
+        for m, n in matches:
+            if m.distance < 0.3 * n.distance:
+                good_matches.append([m])
+
+        img = cv2.drawMatchesKnn(
             frame, kp,
-            matches, good_matches, draw_params)
+            prev_frame, prev_kp,
+            good_matches, None,
+            flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
+        )
+        cv2.imshow('video', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-        # img = fd.draw_good_matches_knn(
-        #     last_frame_image, last_frame_kp,
-        #     frame, kp,
-        #     good_matches,
-        # )
-        # img = cv2.drawMatchesKnn(
-        #     last_frame_image, last_frame_kp,
-        #     frame, kp,
-        #     good_matches, None, **draw_params)
-        cv2.imshow('Lowe', img)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-    last_frame_features = des
-    last_frame_kp = kp
-    last_frame_image = frame
+    prev_kp = kp
+    prev_des = des
+    prev_frame = frame
 
 cap.release()
 cv2.destroyAllWindows()
